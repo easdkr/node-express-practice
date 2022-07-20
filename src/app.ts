536175e5ec78c3bab21cda5b5fedf4718f2b6af5
june <june@JUNE.localdomain>
2022-07-21 08:43:56 +0900

    함수 분리

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,33 @@
 import express, { NextFunction, Request, Response } from "express";
 import {
   SESClient,
-  CloneReceiptRuleSetCommand,
-  CloneReceiptRuleSetCommandInput,
   SendEmailCommand,
-  SendBounceCommandInput,
   SendEmailCommandInput,
   SESClientConfig,
+  Message,
 } from "@aws-sdk/client-ses";
 import { access_key_id, region, secret_access_key } from "./config";
-const app = express();
 
+const app = express();
+const message : Message = {
+  Body: {
+    /* required */
+    Html: {
+      Charset: "UTF-8",
+      Data: "HTML_FORMAT_BODY",
+    },
+    Text: {
+      Charset: "UTF-8",
+      Data: "TEXT_FORMAT_BODY",
+    },
+  },
+  Subject: {
+    Charset: "UTF-8",
+    Data: "EMAIL_SUBJECT",
+  },
+} 
+const sourceEmail = "choie0423@naver.com"
+const destinationEmails = ["choie0423@alphacircle.co.kr",]
 app.get("/", (req: Request, res: Response, nect: NextFunction) => {
   res.send("hello world!");
 });
@@ -30,39 +47,31 @@ app.listen(1234, () => {
 });
 
 const ses = async () => {
+  const sesClient = createSesClient()
+  const sendEmailCommand = createSendEmailCommand()
+  return await sesClient.send(sendEmailCommand);
+};
+
+const createSesClient = () => {
   const config: SESClientConfig = {
     credentials: {
       accessKeyId: access_key_id,
       secretAccessKey: secret_access_key,
     },
-    region,
+    region : region,
   };
-  const sesClient = new SESClient(config);
+
+  return new SESClient(config);
+}
+
+const createSendEmailCommand = () => {
   const sendEmailCommandInput: SendEmailCommandInput = {
     Destination: {
-      ToAddresses: ["choie0423@alphacircle.co.kr"],
+      ToAddresses: destinationEmails,
     },
-    Message: {
-      /* required */
-      Body: {
-        /* required */
-        Html: {
-          Charset: "UTF-8",
-          Data: "HTML_FORMAT_BODY",
-        },
-        Text: {
-          Charset: "UTF-8",
-          Data: "TEXT_FORMAT_BODY",
-        },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: "EMAIL_SUBJECT",
-      },
-    },
-    Source: "choie0423@alphacircle.co.kr",
+    Message: message,
+    Source: sourceEmail
   };
-  const sendEmailCommand = new SendEmailCommand(sendEmailCommandInput);
-  const data = await sesClient.send(sendEmailCommand);
-  return data;
-};
+
+  return new SendEmailCommand(sendEmailCommandInput);
+}
